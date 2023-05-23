68410ed7de47002187ba25397b77d2aa43f5e304
xervzz <89887740+xervzz@users.noreply.github.com>
2023-05-23 14:40:55 +0000

    add knife kills, teamkills

--- routes/leaderboard.js
+++ routes/leaderboard.js
@@ -354,7 +354,7 @@
      sum(v5) as v5, sum(roundsplayed) as trp, sum(flashbang_assists) as fba,
      sum(damage) as dmg, sum(headshot_kills) as hsk, count(id) as totalMaps,
      sum(knife_kills) as knifekills, sum(friendlies_flashed) as fflash,
-     sum(enemies_flashed) as eflash, sum(util_damage) as utildmg
+     sum(enemies_flashed) as eflash, sum(util_damage) as utildmg, sum(teamkills) as tk
      FROM    player_stats
      WHERE   match_id IN (
          SELECT  id
@@ -412,6 +412,8 @@
          v3: parseFloat(player.v3),
          v4: parseFloat(player.v4),
          v5: parseFloat(player.v5),
+         knifekills: parseFloat(player.knifekills),
+         tk: parseFloat(player.tk),
          trp: parseFloat(player.trp),
          fba: parseFloat(player.fba),
          total_damage: parseFloat(player.dmg),
